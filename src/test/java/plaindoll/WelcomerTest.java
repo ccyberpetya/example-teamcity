d8724c2c9f9ffb641a5ae5c16d6c5823e43f716f
ccyberpetya <petya.yudin2@gmail.com>
2026-05-03 18:12:50 +0500

    Add hunter reply method

--- src/test/java/plaindoll/WelcomerTest.java
+++ src/test/java/plaindoll/WelcomerTest.java
@@ -32,4 +32,9 @@ public class WelcomerTest {
 	public void welcomerSaysSomething(){
 		assertThat(welcomer.saySome(), containsString("something"));
 	}
+
+	@Test
+	public void welcomerSaysHunterReply() {
+    assertThat(welcomer.sayHunterReply(), containsString("hunter"));
+
 }
